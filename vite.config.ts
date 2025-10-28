0d3d21949569b347e7d2f26a34e6ee75fdef74a4
tjsdn1204 <tjsdn011204@naver.com>
2025-10-28 16:56:13 +0900

    feat: route setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'  
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     react(),
     tailwindcss()
+    
   ],
   resolve: {
     alias: {
