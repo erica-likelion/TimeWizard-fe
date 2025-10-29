17ca7fbceb34871369851672df835b2d6fb92ad0
tjsdn1204 <tjsdn011204@naver.com>
2025-10-29 22:10:54 +0900

    feat:홈페이지 ui 추가

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,15 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
+
 const RootLayout = () => (
-  <>
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{' '}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr />
+
+  <div className="min-h-screen bg-black text-white">
+    
     <Outlet />
+    
     <TanStackRouterDevtools />
-  </>
+  </div>
 )
 
 export const Route = createRootRoute({ component: RootLayout })
