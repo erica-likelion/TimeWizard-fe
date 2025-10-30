a5395d663133f677d0e26232541a04b8c18c089c
mna11 <thkim306@gmail.com>
2025-10-30 16:45:00 +0900

    feat: sidebar, topbar UI 개발

---
+++ src/components/common/SideBar/index.tsx
@@ -0,0 +1,47 @@
+import { Link } from '@tanstack/react-router';
+import logoIcon from '@assets/icons/billnut_col_noneoutline.svg';
+
+const navItems = [
+    { to: '/home', label: '메인' },
+    { to: '/list', label: '목록' },
+    { to: '/gen', label: '생성' },
+    { to: '/tuning', label: '튜닝' },
+    { to: '/planner', label: '플래너' },
+    { to: '/planb', label: '플랜B' }
+];
+
+const SideBar = () => {
+
+    return (
+        <div className="flex flex-col w-[313px] h-full">
+            <div className="flex items-end px-7.5 w-full h-[160px] bg-[#E65787]">
+                <img
+                    src={logoIcon}
+                    alt="빌넣 로고"
+                    className="h-25"
+                />
+            </div>
+
+            <nav className="my-[37px]">
+                <ul className="flex flex-col">
+                    {navItems.map((item) => (
+                        <li key={item.to}
+                            className="px-7.5 py-4.5">
+                            <Link
+                                to={item.to}
+                                className="text-[28px]"
+                                activeProps={{
+                                    className: "text-[#C1446C]"
+                                }}
+                            >
+                                <p>{item.label}</p>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default SideBar;
