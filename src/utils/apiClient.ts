2c91e89dc9d2f0a3ed552226d01f4c7d8b02ad3e
mna11 <thkim306@gmail.com>
2025-11-11 12:35:45 +0900

    fix: 타입 에러 수정

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import type { InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -11,10 +12,10 @@ const api = axios.create({
 });
 
 
-// 임시 개발 => 추후 로그인 페이지 구현 후 한번 싹다 리팩토링할 예정 
+// 임시 개발 => 추후 로그인 페이지 구현 후 한번 싹다 리팩토링할 예정
 // Request 인터셉터: JWT 토큰을 자동으로 헤더에 추가
 api.interceptors.request.use(
-  (config) => {
+  (config: InternalAxiosRequestConfig) => {
     // localStorage에서 토큰 가져오기
     const token = localStorage.getItem('authToken');
 
@@ -25,17 +26,17 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => {
+  (error: AxiosError) => {
     return Promise.reject(error);
   }
 );
 
 // Response 인터셉터: 401 에러 시 토큰 제거 및 로그인 페이지로 이동
 api.interceptors.response.use(
-  (response) => {
+  (response: AxiosResponse) => {
     return response;
   },
-  (error) => {
+  (error: AxiosError) => {
     // 401 Unauthorized 에러 처리
     if (error.response?.status === 401) {
       // 토큰 제거
