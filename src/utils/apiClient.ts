0b1ce4e35fee4c844cbf53b739ad0af4ca02274a
MNA11 <thkim306@gmail.com>
2025-11-12 00:33:05 +0900

    fix: api 서버 구현에 따른 import 수정

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import type { InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+import type { AxiosError } from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
